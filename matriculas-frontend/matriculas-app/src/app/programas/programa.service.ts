d4fe6a50b70f0d85f91f02e0a49070e36e0aeb5d
sergiovr <sergio.velasquez@globant.com>
2022-06-16 15:31:07 -0500

    se suben mejoras a programas

---
+++ matriculas-frontend/matriculas-app/src/app/programas/programa.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {programa} from './programa';
+import {of, Observable} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class programaService {
+  private endpointUrl = 'http://localhost:8080/api/programas';
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+  constructor(private http: HttpClient ) { }
+
+  getProgramas(): Observable<programa[]>{
+    return this.http.get<programa[]>(this.endpointUrl);
+  }
+
+  getPrograma(codigo: number): Observable<programa>{
+    return this.http.get<programa>(`${this.endpointUrl}/${codigo}`)
+  }
+
+  create(programa: programa): Observable<programa>{
+    return this.http.post<programa>(this.endpointUrl, programa, {headers: this.httpHeaders});
+  }
+  
+  update(programa: programa): Observable<programa>{
+    return this.http.put<programa>(`${this.endpointUrl}/${programa.id}`, programa, {headers: this.httpHeaders})
+  }
+
+  delete(cedula: number): Observable<programa>{
+    return this.http.delete<programa>(`${this.endpointUrl}/${cedula}`, {headers: this.httpHeaders})
+  }
+  
+}
--- matriculas-frontend/matriculas-app/src/app/programas/programa.ts
+++ matriculas-frontend/matriculas-app/src/app/programas/programa.ts
@@ -1,4 +1,4 @@
-export class programas {
+export class programa {
   id: number;
   codigo: string;
   nombre: string;
